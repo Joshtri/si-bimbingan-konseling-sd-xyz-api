22c42661f367e94fe5a292f0ef6c32bae1e5cf98
Joshtri <stuffofyos1516@gmail.com>
2024-07-26 21:07:32 +0800

    create file on project

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import express from "express";
 import { config } from "dotenv";
 
 
-
 config();
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
 
-
-
-
+app.use(express.json());
 
 
 app.listen(PORT,()=>{
